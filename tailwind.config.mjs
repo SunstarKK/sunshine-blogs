adbcead5ff84fc67683d0124b6408b1b7254c004
SunstarK <sunstarkk@proton.me>
2024-05-05 12:54:27 +0300

    2 new pages, CSS theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'teal': '#146c78',
+				'light-teal': '#5a98a0',
+				'background': '#d0e1e4',
+				'white': '#f9f9f9'
+			},
+			fontFamily: {
+				'helvetica-black': ['HelveticaNowText-Black', 'arial']
+			},
+			screens: {
+				'phone': '480px',
+				'tablet': '768px',
+				'laptop': '976px',
+				'desktop': '1440px',
+			}
+		},
 	},
 	plugins: [],
 }
